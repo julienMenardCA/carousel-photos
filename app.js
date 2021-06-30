82ff136fa75476baca28914c1117a272e899bd0a
Julien Menard <julien.menard@campus.academy>
2021-06-30 16:08:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
- 
-app.use(express.static(__dirname + '/public'));
 
 app.use(require("express-session")({
     secret: "Rusty is a dog",
